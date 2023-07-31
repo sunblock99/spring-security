eb01f89c3a7a66fbde8ba330f50c2b00b31db98f
선형채 <sunblock99@kakao.com>
2023-07-31 15:20:51 +0900

    SecurityConfig 생성

---
+++ security/src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+
+        http.formLogin();
+
+        return http.build();
+    }
+}
