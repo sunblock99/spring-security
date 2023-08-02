9f0a7d121ca6c8cbda4a180cd0f0c67e1cfb90f2
선형채 <sunblock99@kakao.com>
2023-08-02 11:01:03 +0900

    remember-me 기능 추가

--- security/src/main/java/com/example/security/SecurityConfig.java
+++ security/src/main/java/com/example/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -22,8 +24,11 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    UserDetailsService userDetailsService;
+
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests()
                 .anyRequest()
@@ -71,6 +76,12 @@ public class SecurityConfig {
                     }
                 })
                 .deleteCookies("remember-me")
+                ;
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService)
                 ;
 
 
